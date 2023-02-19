e7676bec877c3937fcc08249a3e47cac0be97069
eshascc <eslamhashem@gmail.com>
2023-02-19 12:42:31 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,9 +2,9 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - main block
- * discription: get arandom number and print number
- * return 0
+ * main - Determines if a number is positive, negative or zero.
+ *
+ * Return: Always 0 (success)
  */
 int main(void)
 {
