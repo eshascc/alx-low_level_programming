65cb3899221913d46e18f3d8e35930ef6cc3fe16
eshascc <eslamhashem@gmail.com>
2023-02-20 10:21:48 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - main block
+ * Descrpition:  Get a random number and check its
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+		c++;
+	{
+	puchar('\n);
+	return (0);
+}
